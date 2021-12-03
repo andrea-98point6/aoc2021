ec519ea9caebbd51e5d08440cdfc4edf5609fa7c
andrea <andrea@98point6.com>
2021-12-03 08:36:16 -0800

    less vecs

--- src/day3.rs
+++ src/day3.rs
@@ -1,14 +1,13 @@
 pub fn get_consumption(input: Vec<String>) -> i32 {
     let size = input.first().unwrap().len().try_into().unwrap();
 
-    let mut zero_counter: Vec<u32> = vec![0; size];
-    let mut one_counter: Vec<u32> = vec![0; size];
+    let mut one_counter: Vec<i32> = vec![0; size];
 
     // count zeros and one for each location
     input.into_iter().for_each(|string| {
         string.chars().fold(0, |counter, this_bit| {
             if this_bit == '0' {
-                zero_counter[counter] += 1;
+                one_counter[counter] -= 1;
             };
             if this_bit == '1' {
                 one_counter[counter] += 1;
@@ -18,14 +17,13 @@ pub fn get_consumption(input: Vec<String>) -> i32 {
     });
 
     // reduce
-    let (_, epsi, gamma) = zero_counter
+    let (_, epsi, gamma) = one_counter
         .into_iter()
         .rev()
-        .zip(one_counter.into_iter().rev())
-        .fold((0, 0, 0), |(pow_2, epsilon, gamma), (zeros, ones)| {
+        .fold((0, 0, 0), |(pow_2, epsilon, gamma), ones| {
             // the most frequent goes in gamma
             let base: i32 = 2;
-            if ones > zeros {
+            if ones > 0 {
                 (pow_2 + 1, epsilon, gamma + base.pow(pow_2))
             } else {
                 (pow_2 + 1, epsilon + base.pow(pow_2), gamma)
